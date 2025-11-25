123c6c6dae0eaa38ccefa6d83f8a514176c39d60
Zhiqiang Yan <74827816+yanzq95@users.noreply.github.com>
2025-11-25 22:51:45 +0800

    Clean up exports
    Removed PointToMultiViewDepth_pushout from exports.

--- projects/mmdet3d_plugin/datasets/pipelines/__init__.py
+++ projects/mmdet3d_plugin/datasets/pipelines/__init__.py
@@ -3,8 +3,8 @@ from mmdet3d.datasets.pipelines import LoadPointsFromFile
 from mmdet3d.datasets.pipelines import ObjectRangeFilter, ObjectNameFilter
 from .formating import DefaultFormatBundle3D, Collect3D
 from .loading_new import PointToMultiViewDepthandHeight
-from .loading_new_pushout import PointToMultiViewDepth_pushout
+
 __all__ = ['PrepareImageInputs', 'LoadAnnotationsBEVDepth', 'ObjectRangeFilter', 'ObjectNameFilter',
            'PointToMultiViewDepth', 'DefaultFormatBundle3D', 'Collect3D',
-           'PointToMultiViewDepthandHeight', 'PointToMultiViewDepth_pushout']
+           'PointToMultiViewDepthandHeight']
 
